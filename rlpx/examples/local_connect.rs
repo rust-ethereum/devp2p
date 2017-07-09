29af122c02f56bb388e42ab18f80b99201b0dbab
Wei Tang <hi@that.world>
2017-07-09 16:55:08 +0800

    rlpx: print init hello value in local_connect example

--- rlpx/examples/local_connect.rs
+++ rlpx/examples/local_connect.rs
@@ -14,19 +14,28 @@ use etcommon_crypto::SECP256K1;
 use tokio_core::reactor::Core;
 use secp256k1::key::{PublicKey, SecretKey};
 use rand::os::OsRng;
-use futures::Future;
+use futures::future;
+use futures::{Stream, Sink, Future};
 use std::str::FromStr;
-use rlpx::peer::Peer;
+use rlpx::ecies::ECIESStream;
 
-const REMOTE_ID: &str = "88f3c9502af25f6e7b1269016b2147e6a68a39a91bd28203dd4763b5060ba6a66321a3e4a716b434b24f634c8a464f203d1efe83d814d170d7915e92bd06f55b";
+const REMOTE_ID: &str = "2b7ec30900da8c9353a4aa0431ea9be4b7d361a6a71d5c7240f85f06b5d821b423e2673ed9d0c5134e7a6ceba010c0b9d2837c192a7bb14eb06681f0ed2f89fe";
 
 fn main() {
     let addr = "127.0.0.1:30303".parse().unwrap();
     let mut core = Core::new().unwrap();
     let handle = core.handle();
-    let client = Peer::connect_client(
+    let client = ECIESStream::connect(
         &addr, &handle,
         SecretKey::new(&SECP256K1, &mut OsRng::new().unwrap()),
         H512::from_str(REMOTE_ID).unwrap());
-    core.run(client).unwrap();
+    core.run(client
+             .and_then(|client| {
+                 client.into_future().map_err(|(e, _)| e)
+             })
+             .and_then(|(val, client)| {
+                 println!("next value: {:?}", val);
+                 future::ok(client)
+             })
+    ).unwrap();
 }

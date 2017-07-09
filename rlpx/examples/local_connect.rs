809e1373eee4db7bf1b96d623e442d7cd6d9fb41
Wei Tang <hi@that.world>
2017-07-10 01:49:22 +0800

    rlpx: implement RLPxStream

--- rlpx/examples/local_connect.rs
+++ rlpx/examples/local_connect.rs
@@ -18,8 +18,8 @@ use rand::os::OsRng;
 use futures::future;
 use futures::{Stream, Sink, Future};
 use std::str::FromStr;
+use rlpx::{RLPxStream, CapabilityInfo};
 use rlpx::ecies::ECIESStream;
-use rlpx::peer::{PeerStream, HelloMessage, CapabilityInfo};
 
 const REMOTE_ID: &str = "d02c7c6d49c668f750cf6c007b4a9cc96be08c335d3e027afa110f86c48192725aa2e8a60c581044c7c489fee45a3d0acbbfe4d10eb1717bc6b3374364bf895d";
 
@@ -27,18 +27,19 @@ fn main() {
     let addr = "127.0.0.1:30303".parse().unwrap();
     let mut core = Core::new().unwrap();
     let handle = core.handle();
-    let client = PeerStream::connect(
-        &addr, &handle,
+    let client = RLPxStream::new(
         SecretKey::new(&SECP256K1, &mut OsRng::new().unwrap()),
-        H512::from_str(REMOTE_ID).unwrap(),
         4, "etclient Rust/0.1.0".to_string(),
         vec![CapabilityInfo { name: "eth".to_string(), version: 62, length: 8 },
              CapabilityInfo { name: "eth".to_string(), version: 63, length: 17 }],
         0);
-    core.run(client
-             .and_then(|socket| socket.into_future().map_err(|(e, _)| e))
-             .and_then(|(val, socket)| {
+
+    core.run(client.add_peer(&addr, &handle, H512::from_str(REMOTE_ID).unwrap())
+             .and_then(|client| {
+                 client.into_future().map_err(|(e, _)| e)
+             })
+             .and_then(|(val, client)| {
                  println!("val: {:?}", val);
-                 future::ok(socket)
+                 future::ok(client)
              })).unwrap();
 }
--- rlpx/src/lib.rs
+++ rlpx/src/lib.rs
@@ -20,10 +20,172 @@ pub mod peer;
 mod mac;
 mod errors;
 
+pub use peer::{PeerStream, CapabilityInfo};
+
 use bigint::H512;
 use util::pk2id;
 use hash::SECP256K1;
 use secp256k1::key::{PublicKey, SecretKey};
+use futures::future;
+use futures::{Poll, Async, StartSend, AsyncSink, Future, Stream, Sink};
+use std::io;
+use std::net::SocketAddr;
+use tokio_core::reactor::Handle;
+use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_io::codec::{Framed, Encoder, Decoder};
+
+pub struct RLPxStream {
+    streams: Vec<PeerStream>,
+    secret_key: SecretKey,
+    protocol_version: usize,
+    client_version: String,
+    capabilities: Vec<CapabilityInfo>,
+    port: usize,
+}
+
+impl RLPxStream {
+    pub fn new(secret_key: SecretKey, protocol_version: usize,
+               client_version: String, capabilities: Vec<CapabilityInfo>,
+               port: usize) -> RLPxStream {
+        RLPxStream {
+            streams: Vec::new(),
+            secret_key, protocol_version, client_version,
+            capabilities, port
+        }
+    }
+
+    pub fn add_peer(
+        mut self, addr: &SocketAddr, handle: &Handle, remote_id: H512
+    ) -> Box<Future<Item = RLPxStream, Error = io::Error>> {
+        let fu = PeerStream::connect(addr, handle, self.secret_key.clone(),
+                            remote_id, self.protocol_version,
+                            self.client_version.clone(),
+                            self.capabilities.clone(), self.port)
+            .then(move |peer_result| {
+                match peer_result {
+                    Ok(peer) => {
+                        self.streams.push(peer);
+                        future::ok(self)
+                    },
+                    Err(_) => {
+                        future::ok(self)
+                    },
+                }
+            });
+
+        Box::new(fu)
+    }
+}
+
+impl Stream for RLPxStream {
+    type Item = (CapabilityInfo, usize, Vec<u8>);
+    type Error = io::Error;
+
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        'outer: loop {
+            let mut full_iter = true;
+            let mut start_index = 0;
+
+            'inner: for i in start_index..self.streams.len() {
+                match self.streams[i].poll() {
+                    Ok(Async::NotReady) => (),
+                    Ok(Async::Ready(None)) => {
+                        self.streams.swap_remove(i);
+                        full_iter = false;
+                        start_index = i;
+                        break 'inner;
+                    },
+                    Ok(Async::Ready(Some(val))) =>
+                        return Ok(Async::Ready(Some(val))),
+                    Err(e) => {
+                        self.streams.swap_remove(i);
+                        full_iter = false;
+                        start_index = i;
+                        break 'inner;
+                    },
+                }
+            }
+
+            if full_iter {
+                break 'outer;
+            }
+        }
+        Ok(Async::NotReady)
+    }
+}
+
+impl Sink for RLPxStream {
+    type SinkItem = (CapabilityInfo, usize, Vec<u8>);
+    type SinkError = io::Error;
+
+    fn start_send(&mut self, (cap, id, data): (CapabilityInfo, usize, Vec<u8>)) -> StartSend<Self::SinkItem, Self::SinkError> {
+        let mut any_ready = false;
+
+        'outer: loop {
+            let mut full_iter = true;
+            let mut start_index = 0;
+
+            'inner: for i in 0..self.streams.len() {
+                match self.streams[i].start_send((cap.clone(), id, data.clone())) {
+                    Ok(AsyncSink::Ready) => {
+                        any_ready = true;
+                    },
+                    Ok(AsyncSink::NotReady(_)) => (),
+                    Err(e) => {
+                        self.streams.swap_remove(i);
+                        full_iter = false;
+                        start_index = i;
+                        break 'inner;
+                    }
+                }
+            }
+
+            if full_iter {
+                break 'outer;
+            }
+        }
+
+        if any_ready {
+            Ok(AsyncSink::Ready)
+        } else {
+            Ok(AsyncSink::NotReady((cap, id, data)))
+        }
+    }
+
+    fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
+        let mut all_ready = true;
+
+        'outer: loop {
+            let mut full_iter = true;
+            let mut start_index = 0;
+
+            'inner: for i in 0..self.streams.len() {
+                match self.streams[i].poll_complete() {
+                    Ok(Async::Ready(())) => (),
+                    Ok(Async::NotReady) => {
+                        all_ready = false;
+                    },
+                    Err(e) => {
+                        self.streams.swap_remove(i);
+                        full_iter = false;
+                        start_index = i;
+                        break 'inner;
+                    }
+                }
+            }
+
+            if full_iter {
+                break 'outer;
+            }
+        }
+
+        if all_ready {
+            Ok(Async::Ready(()))
+        } else {
+            Ok(Async::NotReady)
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {

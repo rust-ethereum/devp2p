70e4291e449d160ab3e8071f0a5102878685f113
Wei Tang <hi@that.world>
2017-07-09 13:27:14 +0800

    rlpx: implement Peer::connect_client using futures

--- rlpx/src/peer.rs
+++ rlpx/src/peer.rs
@@ -1,10 +1,47 @@
-// pub struct Peer {
-//     client_id: String,
-//     capabilities: Vec<Capability>,
-//     address: SocketAddr,
+use errors::ECIESError;
+use ecies::{ECIESCodec, ECIESValue};
+use futures::future;
+use futures::{Future, Stream, Sink};
+use tokio_core::net::TcpStream;
+use tokio_core::reactor::Handle;
+use tokio_io::AsyncRead;
+use tokio_io::codec::Framed;
+use secp256k1::key::SecretKey;
+use bigint::H512;
+use std::net::SocketAddr;
+use std::io;
 
-//     id: H512,
-//     remote_id: H512,
+pub struct Peer {
+    stream: Framed<TcpStream, ECIESCodec>,
+    remote_id: H512,
+}
 
-//     socket: TcpStream,
-// }
+impl Peer {
+    pub fn connect_client(
+        addr: &SocketAddr, handle: &Handle,
+        secret_key: SecretKey, remote_id: H512
+    ) -> Box<Future<Item = Peer, Error = io::Error>> {
+        let ecies = match ECIESCodec::new_client(secret_key, remote_id) {
+            Ok(val) => val,
+            Err(e) => return Box::new(future::err(
+                io::Error::new(io::ErrorKind::Other, "invalid handshake")))
+                as Box<Future<Item = Peer, Error = io::Error>>,
+        };
+
+        let stream = TcpStream::connect(addr, handle)
+            .and_then(|socket| socket.framed(ecies).send(ECIESValue::Auth))
+            .and_then(|transport| transport.into_future().map_err(|(e, _)| e))
+            .and_then(move |(ack, transport)| {
+                if ack == Some(ECIESValue::Ack) {
+                    Ok(Peer {
+                        stream: transport,
+                        remote_id: remote_id,
+                    })
+                } else {
+                    Err(io::Error::new(io::ErrorKind::Other, "invalid handshake"))
+                }
+            });
+
+        Box::new(stream)
+    }
+}

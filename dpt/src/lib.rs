8272eb1c74fdb8164b23decab2d9561ae95e75ac
Wei Tang <hi@that.world>
2017-07-11 00:42:36 +0800

    dpt: release 0.2.0

--- dpt/src/lib.rs
+++ dpt/src/lib.rs
@@ -1,3 +1,5 @@
+//! Ethereum DPT protocol implementation
+
 extern crate etcommon_bigint as bigint;
 extern crate etcommon_crypto as hash;
 extern crate etcommon_rlp as rlp;
@@ -7,9 +9,316 @@ extern crate secp256k1;
 extern crate futures;
 extern crate tokio_io;
 extern crate tokio_core;
+extern crate time;
 
+mod proto;
 mod message;
 mod util;
+
+use message::*;
+use proto::{DPTCodec, DPTCodecMessage};
+use futures::future;
+use futures::{Poll, Async, StartSend, AsyncSink, Future, Stream, Sink};
+use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_io::codec::{Framed, Encoder, Decoder};
+use tokio_core::reactor::{Timeout, Handle};
+use tokio_core::net::{UdpSocket, UdpFramed};
+use std::net::{IpAddr, SocketAddr, Ipv4Addr, Ipv6Addr};
+use std::io;
+use bigint::{H256, H512};
+use rlp::UntrustedRlp;
+use secp256k1::key::SecretKey;
+
+fn retain_mut<T, F>(vec: &mut Vec<T>, mut f: F)
+    where F: FnMut(&mut T) -> bool
+{
+    let len = vec.len();
+    let mut del = 0;
+    {
+        let v = &mut **vec;
+
+        for i in 0..len {
+            if !f(&mut v[i]) {
+                del += 1;
+            } else if del > 0 {
+                v.swap(i - del, i);
+            }
+        }
+    }
+    if del > 0 {
+        vec.truncate(len - del);
+    }
+}
+
+/// DPT message for requesting new peers or ping with timeout
+pub enum DPTMessage {
+    RequestNewPeer,
+    Ping(Timeout),
+}
+
+/// DPT stream for sending DPT messages or receiving new peers
+pub struct DPTStream {
+    stream: UdpFramed<DPTCodec>,
+    id: H512,
+    connected: Vec<DPTNode>,
+    timeout: Option<(Timeout, Vec<H512>)>,
+    address: IpAddr,
+    udp_port: u16,
+    tcp_port: u16,
+}
+
+#[derive(Debug, Clone)]
+/// DPT node used by a DPT stream
+pub struct DPTNode {
+    pub address: IpAddr,
+    pub tcp_port: u16,
+    pub udp_port: u16,
+    pub id: H512,
+}
+
+impl DPTNode {
+    /// The TCP socket address of this node
+    pub fn tcp_addr(&self) -> SocketAddr {
+        SocketAddr::new(self.address, self.tcp_port)
+    }
+
+    /// The UDP socket address of this node
+    pub fn udp_addr(&self) -> SocketAddr {
+        SocketAddr::new(self.address, self.udp_port)
+    }
+}
+
+impl DPTStream {
+    /// Create a new DPT stream
+    pub fn new(addr: &SocketAddr, handle: &Handle,
+               secret_key: SecretKey, id: H512,
+               bootstrap_nodes: Vec<DPTNode>,
+               tcp_port: u16) -> Result<Self, io::Error> {
+        Ok(Self {
+            stream: UdpSocket::bind(addr, handle)?.framed(DPTCodec::new(secret_key)),
+            id, connected: bootstrap_nodes,
+            timeout: None,
+            address: addr.ip(), udp_port: addr.port(), tcp_port
+        })
+    }
+}
+
+impl Stream for DPTStream {
+    type Item = Vec<DPTNode>;
+    type Error = io::Error;
+
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        let mut timeoutted = false;
+        if self.timeout.is_some() {
+            let &mut (ref mut timeout, ref hs) = self.timeout.as_mut().unwrap();
+            timeoutted = match timeout.poll() {
+                Ok(Async::Ready(())) => true,
+                Ok(Async::NotReady) => false,
+                Err(e) => return Err(e),
+            };
+
+            if timeoutted {
+                for h in hs {
+                    self.connected.retain(|v| v.id != *h);
+                }
+            }
+        }
+        if timeoutted {
+            self.timeout = None;
+        }
+
+        let (message, remote_id, hash) = match try_ready!(self.stream.poll()) {
+            Some(Some(val)) => val,
+            Some(None) => return Ok(Async::NotReady),
+            None => return Ok(Async::Ready(None)),
+        };
+
+        match message.typ {
+            0x01 /* ping */ => {
+                let incoming_message: PingMessage = match UntrustedRlp::new(&message.data).as_val() {
+                    Ok(val) => val,
+                    Err(_) => return Ok(Async::NotReady),
+                };
+                let typ = 0x02u8;
+                let echo = hash;
+                let to = incoming_message.from;
+                let outgoing_message = PongMessage {
+                    echo, to, timestamp: time::now_utc().to_timespec().sec as u64,
+                };
+                let data = rlp::encode(&outgoing_message).to_vec();
+
+                // If sending pong is not available, drop.
+                self.stream.start_send(DPTCodecMessage {
+                    typ, data, addr: message.addr
+                });
+
+                return Ok(Async::NotReady);
+            },
+            0x02 /* pong */ => {
+                let incoming_message: PongMessage = match UntrustedRlp::new(&message.data).as_val() {
+                    Ok(val) => val,
+                    Err(_) => return Ok(Async::NotReady),
+                };
+                if self.timeout.is_some() {
+                    self.timeout.as_mut().unwrap().1.retain(|v| {
+                        *v != remote_id
+                    });
+                }
+
+                return Ok(Async::NotReady);
+            },
+            0x03 /* find neighbours */ => {
+                // Return at most 3 nodes at a time.
+                let mut nodes = Vec::new();
+                for i in 0..self.connected.len() {
+                    if nodes.len() >= 3 {
+                        break;
+                    }
+
+                    let address = self.connected[i].address;
+                    let udp_port = self.connected[i].udp_port;
+                    let tcp_port = self.connected[i].tcp_port;
+                    let id = self.connected[i].id;
+
+                    nodes.push(Neighbour {
+                        address, udp_port, tcp_port, id,
+                    });
+                }
+                let typ = 0x04u8;
+                let outgoing_message = NeighboursMessage {
+                    nodes, timestamp: time::now_utc().to_timespec().sec as u64,
+                };
+                let data = rlp::encode(&outgoing_message).to_vec();
+
+                self.stream.start_send(DPTCodecMessage {
+                    typ, data, addr: message.addr
+                });
+
+                return Ok(Async::NotReady);
+            },
+            0x04 /* neighbours */ => {
+                let incoming_message: NeighboursMessage = match UntrustedRlp::new(&message.data).as_val() {
+                    Ok(val) => val,
+                    Err(_) => return Ok(Async::NotReady),
+                };
+                let mut ret = Vec::new();
+                for i in 0..incoming_message.nodes.len() {
+                    if self.connected.iter()
+                        .all(|node| node.id != incoming_message.nodes[i].id)
+                    {
+                        let ip = incoming_message.nodes[i].address;
+                        let tcp_port = incoming_message.nodes[i].tcp_port;
+                        let udp_port = incoming_message.nodes[i].udp_port;
+                        let remote_id = incoming_message.nodes[i].id;
+                        let addr = SocketAddr::new(ip, udp_port);
+                        let node = DPTNode {
+                            address: ip,
+                            udp_port, tcp_port,
+                            id: remote_id
+                        };
+                        self.connected.push(node.clone());
+                        ret.push(node);
+                    }
+                }
+                if ret.len() > 0 {
+                    return Ok(Async::Ready(Some(ret)));
+                } else {
+                    return Ok(Async::NotReady);
+                }
+            },
+            _ => {
+                return Ok(Async::NotReady);
+            }
+        }
+    }
+}
+
+impl Sink for DPTStream {
+    type SinkItem = DPTMessage;
+    type SinkError = io::Error;
+
+    fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
+        self.stream.poll_complete()
+    }
+
+    fn start_send(&mut self, message: DPTMessage) -> StartSend<Self::SinkItem, Self::SinkError> {
+        match message {
+
+            DPTMessage::RequestNewPeer => {
+                let mut any_sent = false;
+                let typ = 0x03u8;
+                let message = FindNeighboursMessage {
+                    id: self.id,
+                    timestamp: time::now_utc().to_timespec().sec as u64,
+                };
+                let data = rlp::encode(&message).to_vec();
+                let ref mut connected = self.connected;
+                let ref mut stream = self.stream;
+                retain_mut(connected, |node| {
+                    match stream.start_send(DPTCodecMessage {
+                        addr: node.udp_addr(), typ, data: data.clone()
+                    }) {
+                        Ok(AsyncSink::Ready) => {
+                            any_sent = true;
+                            true
+                        },
+                        Ok(AsyncSink::NotReady(_)) => true,
+                        Err(_) => false,
+                    }
+                });
+                if any_sent {
+                    return Ok(AsyncSink::Ready);
+                } else {
+                    return Ok(AsyncSink::NotReady(DPTMessage::RequestNewPeer));
+                }
+            },
+
+            DPTMessage::Ping(timeout) => {
+                let mut any_sent = false;
+                let mut timeouting = Vec::new();
+                let from_endpoint = Endpoint {
+                    address: self.address.clone(),
+                    udp_port: self.udp_port,
+                    tcp_port: self.tcp_port,
+                };
+                let ref mut connected = self.connected;
+                let ref mut stream = self.stream;
+                retain_mut(connected, |node| {
+                    let typ = 0x01u8;
+                    let (address, tcp_port, udp_port, remote_id) =
+                        (node.address, node.tcp_port, node.udp_port, node.id);
+                    let message = PingMessage {
+                        version: H256::from(0x3),
+                        from: from_endpoint.clone(),
+                        to: Endpoint {
+                            address, udp_port, tcp_port
+                        },
+                        timestamp: time::now_utc().to_timespec().sec as u64,
+                    };
+                    let data = rlp::encode(&message).to_vec();
+                    match stream.start_send(DPTCodecMessage {
+                        addr: node.udp_addr(), typ, data
+                    }) {
+                        Ok(AsyncSink::Ready) => {
+                            any_sent = true;
+                            timeouting.push(remote_id);
+                            true
+                        },
+                        Ok(AsyncSink::NotReady(_)) => true,
+                        Err(_) => false,
+                    }
+                });
+                if any_sent {
+                    self.timeout = Some((timeout, timeouting));
+                    return Ok(AsyncSink::Ready);
+                } else {
+                    return Ok(AsyncSink::NotReady(DPTMessage::Ping(timeout)));
+                }
+            }
+
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- dpt/src/message.rs
+++ dpt/src/message.rs
@@ -1,88 +1,188 @@
-use util::{keccak256, pk2id, id2pk};
-use hash::SECP256K1;
-use std::net::SocketAddr;
-use secp256k1::key::SecretKey;
-use secp256k1::{RecoverableSignature, Message, RecoveryId};
+use rlp::{Encodable, Decodable, DecoderError, UntrustedRlp, RlpStream};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use bigint::{H256, H512};
-use tokio_core::net::UdpCodec;
-use std::io;
 
-macro_rules! try_none {
-    ( $ex:expr ) => {
-        match $ex {
-            Ok(val) => val,
-            Err(_) => return Ok(None),
-        }
+pub struct Neighbour {
+    pub address: IpAddr,
+    pub udp_port: u16,
+    pub tcp_port: u16,
+    pub id: H512
+}
+
+impl Encodable for Neighbour {
+    fn rlp_append(&self, s: &mut RlpStream) {
+        s.begin_list(4);
+        let address: Vec<u8> = match self.address.clone() {
+            IpAddr::V4(v) => v.octets().as_ref().into(),
+            IpAddr::V6(v) => v.octets().as_ref().into(),
+        };
+        s.append(&address);
+        s.append(&self.udp_port);
+        s.append(&self.tcp_port);
+        s.append(&self.id);
     }
 }
 
-pub struct DPTCodec {
-    secret_key: SecretKey,
-}
-
-pub struct DPTMessage {
-    pub addr: SocketAddr,
-    pub typ: u8,
-    pub data: Vec<u8>
-}
-
-impl UdpCodec for DPTCodec {
-    type In = Option<(DPTMessage, H512)>;
-    type Out = DPTMessage;
-
-    fn decode(&mut self, src: &SocketAddr, buf: &[u8]) -> Result<Self::In, io::Error> {
-        if buf.len() < 98 {
-            return Ok(None);
-        }
-
-        let hash = keccak256(&buf[32..]);
-        let check_hash = H256::from(&buf[0..32]);
-        if check_hash != hash {
-            return Ok(None);
-        }
-
-        let sighash = keccak256(&buf[97..]);
-        let rec_id = try_none!(RecoveryId::from_i32(buf[96] as i32));
-        let rec_sig = try_none!(RecoverableSignature::from_compact(
-            &SECP256K1, &buf[32..96], rec_id
-        ));
-        let message = try_none!(Message::from_slice(&sighash));
-        let public_key = try_none!(SECP256K1.recover(&message, &rec_sig));
-        let remote_id = pk2id(&public_key);
-
-        let typ = buf[97];
-        let mut data = Vec::new();
-        for i in 98..buf.len() {
-            data.push(buf[i]);
-        }
-
-        Ok(Some((DPTMessage { addr: src.clone(), typ, data }, remote_id)))
+impl Decodable for Neighbour {
+    fn decode(rlp: &UntrustedRlp) -> Result<Self, DecoderError> {
+        let address_raw: Vec<u8> = rlp.val_at(0)?;
+        let address = if address_raw.len() == 4 {
+            IpAddr::V4(Ipv4Addr::new(address_raw[0], address_raw[1],
+                                     address_raw[2], address_raw[3]))
+        } else if address_raw.len() == 16 {
+            let mut raw = [0u8; 16];
+            for i in 0..16 {
+                raw[i] = address_raw[i];
+            }
+            IpAddr::from(raw)
+        } else {
+            return Err(DecoderError::Custom("wrong address length"));
+        };
+        Ok(Self {
+            address,
+            udp_port: rlp.val_at(1)?,
+            tcp_port: rlp.val_at(2)?,
+            id: rlp.val_at(3)?,
+        })
     }
+}
 
-    fn encode(&mut self, mut msg: DPTMessage, buf: &mut Vec<u8>) -> SocketAddr {
-        let mut typdata = Vec::new();
-        typdata.push(msg.typ);
-        typdata.append(&mut msg.data);
+#[derive(Debug, Clone)]
+pub struct Endpoint {
+    pub address: IpAddr,
+    pub udp_port: u16,
+    pub tcp_port: u16,
+}
 
-        let sighash = keccak256(&typdata);
-        let message = Message::from_slice(&sighash).unwrap();
-        let rec_sig = &SECP256K1.sign_recoverable(&message, &self.secret_key).unwrap();
-        let (rec, sig) = rec_sig.serialize_compact(&SECP256K1);
+impl Encodable for Endpoint {
+    fn rlp_append(&self, s: &mut RlpStream) {
+        s.begin_list(3);
+        let address: Vec<u8> = match self.address.clone() {
+            IpAddr::V4(v) => v.octets().as_ref().into(),
+            IpAddr::V6(v) => v.octets().as_ref().into(),
+        };
+        s.append(&address);
+        s.append(&self.udp_port);
+        s.append(&self.tcp_port);
+    }
+}
 
-        let mut hashdata = Vec::new();
-        for d in sig.as_ref() {
-            hashdata.push(*d);
-        }
-        hashdata.push(rec.to_i32() as u8);
-        hashdata.append(&mut typdata);
+impl Decodable for Endpoint {
+    fn decode(rlp: &UntrustedRlp) -> Result<Self, DecoderError> {
+        let address_raw: Vec<u8> = rlp.val_at(0)?;
+        let address = if address_raw.len() == 4 {
+            IpAddr::V4(Ipv4Addr::new(address_raw[0], address_raw[1],
+                                     address_raw[2], address_raw[3]))
+        } else if address_raw.len() == 16 {
+            let mut raw = [0u8; 16];
+            for i in 0..16 {
+                raw[i] = address_raw[i];
+            }
+            IpAddr::from(raw)
+        } else {
+            return Err(DecoderError::Custom("wrong address length"));
+        };
+        Ok(Self {
+            address,
+            udp_port: rlp.val_at(1)?,
+            tcp_port: rlp.val_at(2)?,
+        })
+    }
+}
 
-        let hash = keccak256(&hashdata);
+pub struct FindNeighboursMessage {
+    pub id: H512,
+    pub timestamp: u64,
+}
 
-        for i in 0..32 {
-            buf.push(hash[i]);
-        }
-        buf.append(&mut hashdata);
+impl Encodable for FindNeighboursMessage {
+    fn rlp_append(&self, s: &mut RlpStream) {
+        s.begin_list(2);
+        s.append(&self.id);
+        s.append(&self.timestamp);
+    }
+}
 
-        msg.addr
+impl Decodable for FindNeighboursMessage {
+    fn decode(rlp: &UntrustedRlp) -> Result<Self, DecoderError> {
+        Ok(Self {
+            id: rlp.val_at(0)?,
+            timestamp: rlp.val_at(1)?,
+        })
+    }
+}
+
+pub struct NeighboursMessage {
+    pub nodes: Vec<Neighbour>,
+    pub timestamp: u64,
+}
+
+impl Encodable for NeighboursMessage {
+    fn rlp_append(&self, s: &mut RlpStream) {
+        s.begin_list(2);
+        s.append_list(&self.nodes);
+        s.append(&self.timestamp);
+    }
+}
+
+impl Decodable for NeighboursMessage {
+    fn decode(rlp: &UntrustedRlp) -> Result<Self, DecoderError> {
+        Ok(Self {
+            nodes: rlp.list_at(0)?,
+            timestamp: rlp.val_at(1)?,
+        })
+    }
+}
+
+pub struct PingMessage {
+    pub version: H256,
+    pub from: Endpoint,
+    pub to: Endpoint,
+    pub timestamp: u64,
+}
+
+impl Encodable for PingMessage {
+    fn rlp_append(&self, s: &mut RlpStream) {
+        s.begin_list(4);
+        s.append(&self.version);
+        s.append(&self.from);
+        s.append(&self.to);
+        s.append(&self.timestamp);
+    }
+}
+
+impl Decodable for PingMessage {
+    fn decode(rlp: &UntrustedRlp) -> Result<Self, DecoderError> {
+        Ok(Self {
+            version: rlp.val_at(0)?,
+            from: rlp.val_at(1)?,
+            to: rlp.val_at(2)?,
+            timestamp: rlp.val_at(3)?,
+        })
+    }
+}
+
+pub struct PongMessage {
+    pub to: Endpoint,
+    pub echo: H256,
+    pub timestamp: u64,
+}
+
+impl Encodable for PongMessage {
+    fn rlp_append(&self, s: &mut RlpStream) {
+        s.begin_list(3);
+        s.append(&self.to);
+        s.append(&self.echo);
+        s.append(&self.timestamp);
+    }
+}
+
+impl Decodable for PongMessage {
+    fn decode(rlp: &UntrustedRlp) -> Result<Self, DecoderError> {
+        Ok(Self {
+            to: rlp.val_at(0)?,
+            echo: rlp.val_at(1)?,
+            timestamp: rlp.val_at(2)?,
+        })
     }
 }

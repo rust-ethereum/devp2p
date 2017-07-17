eda50786b40e658bc5d8427dbe235bc9dfc955a1
Wei Tang <hi@that.world>
2017-07-17 12:11:08 +0800

    dpt, local_connect: use a public bootstrap node

--- dpt/examples/local_connect.rs
+++ dpt/examples/local_connect.rs
@@ -20,28 +20,32 @@ use futures::{Stream, Sink, Future};
 use std::str::FromStr;
 use dpt::{DPTMessage, DPTNode, DPTStream};
 
-const REMOTE_ID: &str = "d02c7c6d49c668f750cf6c007b4a9cc96be08c335d3e027afa110f86c48192725aa2e8a60c581044c7c489fee45a3d0acbbfe4d10eb1717bc6b3374364bf895d";
+const REMOTE_ID: &str = "42d8f29d1db5f4b2947cd5c3d76c6d0d3697e6b9b3430c3d41e46b4bb77655433aeedc25d4b4ea9d8214b6a43008ba67199374a9b53633301bca0cd20c6928ab";
 
 fn main() {
-    let addr = "127.0.0.1:50505".parse().unwrap();
+    let addr = "0.0.0.0:50505".parse().unwrap();
     let mut core = Core::new().unwrap();
     let handle = core.handle();
     let mut client = DPTStream::new(
         &addr, &handle,
         SecretKey::new(&SECP256K1, &mut OsRng::new().unwrap()),
         vec![DPTNode {
-            address: "127.0.0.1".parse().unwrap(),
+            address: "104.155.176.151".parse().unwrap(),
             tcp_port: 30303,
             udp_port: 30303,
             id: H512::from_str(REMOTE_ID).unwrap(),
         }], 0).unwrap();
 
-    core.run(client.send(DPTMessage::RequestNewPeer)
-             .and_then(|client| {
-                 client.into_future().map_err(|(e, _)| e)
-             })
-             .and_then(|(val, client)| {
-                 println!("new peer: {:?}", val);
-                 future::ok(client)
-             })).unwrap();
+    let result = core.run(client.send(DPTMessage::RequestNewPeer)
+                          .and_then(|client| {
+                              client.into_future().map_err(|(e, _)| e)
+                          })
+                          .and_then(|(val, client)| {
+                              println!("new peer: {:?}", val);
+                              future::ok(client)
+                          }));
+
+    if result.is_err() {
+        println!("err: {:?}", result.err());
+    }
 }

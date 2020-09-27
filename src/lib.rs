060a6b2a46a49de178633d09ec0398270a242cc3
Artem Vorotnikov <artem@vorotnikov.me>
2020-09-27 23:44:31 +0300

    Fix clippy warning level

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,7 @@
 //! * `EthIngressServer` which `MuxServer` calls into when new requests and gossip messages arrive.
 //! * `MuxServer` itself implements `EthProtocol` which is a simple gateway to abstract Ethereum network.
 
-#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
-#![allow(
-    unreachable_code,
-    clippy::cast_possible_truncation,
-    clippy::default_trait_access,
-    clippy::filter_map,
-    clippy::if_not_else,
-    clippy::missing_errors_doc,
-    clippy::module_name_repetitions,
-    clippy::too_many_arguments,
-    clippy::too_many_lines,
-    clippy::used_underscore_binding,
-    clippy::wildcard_imports
-)]
+#![allow(clippy::large_enum_variant)]
 
 mod disc;
 pub mod ecies;

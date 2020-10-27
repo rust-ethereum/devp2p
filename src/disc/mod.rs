f7e355ec23bbe24fd695775432f84f3dce43148a
Artem Vorotnikov <artem@vorotnikov.me>
2020-10-27 07:35:12 +0300

    Discovery trait should require Sync

--- src/disc/mod.rs
+++ src/disc/mod.rs
@@ -12,6 +12,9 @@ pub use self::discv4::Discv4;
 #[cfg(feature = "discv5")]
 mod discv5;
 
+#[cfg(feature = "discv5")]
+pub use self::discv5::Discv5;
+
 #[cfg(feature = "dnsdisc")]
 mod dnsdisc;
 
@@ -19,12 +22,12 @@ mod dnsdisc;
 pub use self::dnsdisc::DnsDiscovery;
 
 #[async_trait]
-pub trait Discovery: Send + Sync + 'static {
+pub trait Discovery: Send + 'static {
     async fn get_new_peer(&mut self) -> anyhow::Result<NodeRecord>;
 }
 
 #[async_trait]
-impl<S: Send + Sync + 'static> Discovery for HashMap<SocketAddr, PeerId, S> {
+impl<S: Send + 'static> Discovery for HashMap<SocketAddr, PeerId, S> {
     async fn get_new_peer(&mut self) -> anyhow::Result<NodeRecord> {
         Ok(self
             .iter()
